019f663566ade63e149edcca956f4be2145dae58
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2020-10-30 17:30:32 +0100

    basic component structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,38 @@
+import { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        tittle: '',
+        completed: false
+      },
+      {
+        id: 2,
+        tittle: '',
+        completed: false
+      },
+      {
+        id: 3,
+        tittle: '',
+        completed: false
+      }
+    ]
+  }
+  render(){
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <h1>App 2</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
+    
+  
 }
 
 export default App;
