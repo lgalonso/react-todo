e3e005d59727941f90f732fbc446c0acdfd4bf7c
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2020-10-30 19:16:42 +0100

    AddTodo structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,62 +1,69 @@
 import { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
-import Header from './components/layout/Header'
-
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 class App extends Component {
   state = {
     app_version: "v0.2",
     todos: [
       {
         id: 1,
-        tittle: 'todo 1',
-        completed: false
+        tittle: "todo 1",
+        completed: false,
       },
       {
         id: 2,
-        tittle: 'todo 2',
-        completed: false
+        tittle: "todo 2",
+        completed: false,
       },
       {
         id: 3,
-        tittle: 'todo 3',
-        completed: false
-      }
-    ]
-  }
+        tittle: "todo 3",
+        completed: false,
+      },
+    ],
+  };
 
   /** By using arrow functions to define our methods we avoid having to bind them everytime we pass them down to a component
-   * 
-   * @param {*} id 
+   *
+   * @param {*} id
    *
    */
   toggleComplete = (id) => {
-        this.setState({ todos: this.state.todos.map(todo => {
-          if(todo.id === id) todo.completed = !todo.completed
-          return todo;
-        }) 
-      })
-  }
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) todo.completed = !todo.completed;
+        return todo;
+      }),
+    });
+  };
 
   /**
-   * 
-   * @param {*} id 
+   *
+   * @param {*} id
    */
   deleteTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
-}
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
 
-  render(){
+  render() {
     return (
       <div className="App">
-        <Header app_version={this.state.app_version}></Header>
-        {/* <h1>Todo App {this.state.app_version}</h1> */}
-        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} deleteTodo={this.deleteTodo}/>
+        <div className="container">
+          <Header app_version={this.state.app_version}></Header>
+          <AddTodo></AddTodo>
+          <Todos
+            todos={this.state.todos}
+            toggleComplete={this.toggleComplete}
+            deleteTodo={this.deleteTodo}
+          />
+        </div>
       </div>
     );
   }
-    
-  
 }
 
 export default App;
