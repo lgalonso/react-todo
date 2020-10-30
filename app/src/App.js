f4d1c56167c0e5d4a7ef1732eb76685b3458b278
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2020-10-30 17:42:54 +0100

    app comp structure

--- app/src/App.js
+++ app/src/App.js
@@ -4,20 +4,21 @@ import Todos from './components/Todos';
 
 class App extends Component {
   state = {
+    app_version: "v0.1",
     todos: [
       {
         id: 1,
-        tittle: '',
+        tittle: 'todo 1',
         completed: false
       },
       {
         id: 2,
-        tittle: '',
+        tittle: 'todo 2',
         completed: false
       },
       {
         id: 3,
-        tittle: '',
+        tittle: 'todo 3',
         completed: false
       }
     ]
@@ -26,7 +27,7 @@ class App extends Component {
     console.log(this.state.todos)
     return (
       <div className="App">
-        <h1>App 2</h1>
+        <h1>Todo App {this.state.app_version}</h1>
         <Todos todos={this.state.todos}/>
       </div>
     );
